f1755a2b5176ed6fea892e4eee409f3bdb46e9e1
Emirhan Pişkin <emirhan.piskin50@gmail.com>
2025-09-16 14:01:52 +0300

    fix

--- iyilik-pinari-server/src/index.ts
+++ iyilik-pinari-server/src/index.ts
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-    'https://iyilik-pinari.vercel.app',
+    'https://iyilik-pinari-git-main-emirhans-projects-7c718bb7.vercel.app/',
 ];
 const corsOptions = {
     origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
